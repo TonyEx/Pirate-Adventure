ad4064c2a6a268a427b173d19bf08a1a4c3edce3
Tony Angelo <tonyangelox@veronica.home>
2014-06-09 21:27:04 -0400

    Initial Commit

---
+++ Pirate Adventure/TEAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAAppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Tony Angelo on 6/9/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Adventure/TEAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEAViewController.h
+//  Pirate Adventure
+//
+//  Created by Tony Angelo on 6/9/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAViewController : UIViewController
+
+@end
